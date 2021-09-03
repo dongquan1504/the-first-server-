735707c4e61de4a9605d0089d05ce2f7212ca366
Duong Dong Quan <dongquan15041@gmail.com>
2021-09-03 22:19:50 +0700

    update connection

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const login = require("./controllers/login");
 const image = require("./controllers/image");
 const profile = require("./controllers/profile");
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: true,
   },
 });
 
