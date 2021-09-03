64ce2845b1cca40cf43fee377fdd93ce09e1d7d2
Duong Dong Quan <dongquan15041@gmail.com>
2021-09-03 19:30:36 +0700

    update connection

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ const profile = require("./controllers/profile");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "dev",
-    password: "",
-    database: "smart-brain",
+    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
   },
 });
 
